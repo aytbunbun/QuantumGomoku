36460d2769ba38162c9ecf8aa45ffda070e58eee
aytbunbun <s1F102301000@iniad.org>
2024-05-01 01:39:34 +0900

    4色置けるように変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const board = [
+  const [board, setBoard] = useState([
+    [1, 2, 3, 4, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -16,13 +18,43 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
+  ]);
+  const [turnColor, setTurnColor] = useState(3);
+  const clickCell = (x: number, y: number) => {
+    const newBoard = structuredClone(board);
+    if (newBoard[y][x] === 0) {
+      newBoard[y][x] = turnColor;
+      setBoard(newBoard);
+      setTurnColor(turnColor === 3 ? 6 : turnColor === 4 ? 5 : turnColor === 5 ? 3 : 4);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
-          row.map((color, x) => <div className={styles.cell} key={`${x}-${y}`} />),
+          row.map((color, x) => (
+            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickCell(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{
+                    backgroundColor:
+                      color === 1
+                        ? '#000000'
+                        : color === 2
+                          ? '#ffffff'
+                          : color === 3
+                            ? '#333333'
+                            : color === 4
+                              ? '#4d4d4d'
+                              : color === 5
+                                ? '#b3b3b3'
+                                : '#e6e6e6',
+                  }}
+                />
+              )}
+            </div>
+          )),
         )}
       </div>
     </div>
